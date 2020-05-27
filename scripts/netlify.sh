45c7d22437fb3c9744e40eb85e82b866aa046072
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-27 14:04:01 +0200

    chore: updated netlify script

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -4,16 +4,21 @@ echo "--------------------------------------------------"
 echo "1 - Install SecretHub"
 echo "--------------------------------------------------"
 
-# mkdir -p ./secrethub
-# wget --quiet https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secrethub-v0.37.0-linux-amd64.tar.gz
-# tar -zxvf secrethub-v0.37.0-linux-amd64.tar.gz -C ./secrethub
-# chmod +x secrethub/bin/secrethub
-# rm secrethub-v0.37.0-linux-amd64.tar.gz
-# export PATH="$PWD/secrethub/bin:$PATH"
+mkdir -p ./secrethub
+wget --quiet https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secrethub-v0.37.0-linux-amd64.tar.gz
+tar -zxvf secrethub-v0.37.0-linux-amd64.tar.gz -C ./secrethub
+chmod +x secrethub/bin/secrethub
+rm secrethub-v0.37.0-linux-amd64.tar.gz
+export PATH="$PWD/secrethub/bin:$PATH"
 
 echo "--------------------------------------------------"
-echo "2 - Build"
+echo "2 - Load secrets"
 echo "--------------------------------------------------"
 
-# secrethub run --var env=$SECRETHUB_ENV -- yarn build
+secrethub inject -i secrethub.env -o .env
+
+echo "--------------------------------------------------"
+echo "3 - Build"
+echo "--------------------------------------------------"
+
 yarn build
